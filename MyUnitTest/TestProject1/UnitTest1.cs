940d73340a1b5a1df11d9e9ea4c347b45aefb8f9
ReginaTahtarova <regi.rr@mail.ru>
2015-12-17 11:40:40 +0300

    MyProject
    unittest

---
+++ MyUnitTest/TestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyUnitTest;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var actual = Class1.Function(5, 2);
+            Assert.AreNotEqual(actual.Length, 0);
+        }
+    }
+}
